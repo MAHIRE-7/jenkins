981875a105c6dba96e73a8d2996e448ebe01f789
manoday <manodayahire786@gmail.com>
2026-01-29 15:31:30 +0530

    Fix: Shellscript files

--- A shellscript-to-install -jenkins/agent.sh
+++ A shellscript-to-install -jenkins/agent.sh
@@ -1,38 +1,51 @@
 #!/bin/bash
 
 #Installing Java
-echo "Installing Java"
+echo -e "\033[32mInstalling Java\033[0m"
 sudo apt update
 sudo apt install fontconfig openjdk-21-jre -y
 java -version
-echo "Java Installation completed"
+echo -e "\033[32mJava Installation completed\033[0m"
 sleep 5
 
 #Installing Docker
-echo "Installing Docker on Agent"
+echo -e "\033[32mInstalling Docker on Agent\033[0m"
 sudo apt-get update
 sudo apt-get install docker.io -y
 sudo usermod -aG docker $USER
 sudo newgrp docker
 echo "Please log out and back in for Docker group changes to take effect"
 
-echo "Installation complete"
-sleep 2
+echo -e "\033[32mInstallation complete\033[0m"
+sleep 5
 
 echo "Do you want to install SonarQube Scanner? (y/n): "
 read -r choice
 
 
 if [[ $choice == "y" || $choice == "Y" ]]; then
-    echo "Installing SonarQube Scanner..."
-    sudo apt install unzip -y
-    wget https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-5.0.1.3006-linux.zip
-    unzip sonar-scanner-5.0.1.3006-linux.zip
-    sudo mv sonar-scanner-5.0.1.3006-linux /opt/sonar-scanner
-    sudo ln -s /opt/sonar-scanner/bin/sonar-scanner /usr/bin/sonar-scanner
-    echo "SonarQube Scanner installed successfully!"
+    echo -e "\033[32mInstalling SonarQube Scanner...\033[0m"
+    sudo apt update
+    sudo apt install curl unzip -y
+    cd /opt
+    sudo curl -fL -o sonar-scanner.zip \
+    https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-5.0.1.3006-linux.zip
+    
+    if [ $? -eq 0 ]; then
+        sudo unzip sonar-scanner.zip
+        sudo mv sonar-scanner-5.0.1.3006-linux sonar-scanner
+        sudo ln -s /opt/sonar-scanner/bin/sonar-scanner /usr/bin/sonar-scanner
+        sudo rm sonar-scanner.zip
+        echo -e "\033[32mSonarQube Scanner installed successfully!\033[0m"
+    else
+        echo "Failed to download SonarQube Scanner"
+    fi
 else
     echo "SonarQube Scanner installation skipped."
 fi
 
-echo "Jenkins Agent Setup Completed successfully"
+echo -e "\033[32mJenkins Agent Setup Completed successfully\033[0m"
+
+sleep 10 
+
+sudo reboot
--- A shellscript-to-install -jenkins/k8s.sh
+++ A shellscript-to-install -jenkins/k8s.sh
@@ -1,17 +1,45 @@
 #!/bin/bash
 #AWS CLI
+echo -e "\033[32mInstalling AWS CLI\033[0m"
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-sudo apt install unzip
+sudo apt install unzip -y
 unzip awscliv2.zip
 sudo ./aws/install
+rm -rf awscliv2.zip aws/
+sleep 5
 
 #KUBECTL
+echo -e "\033[32mInstalling Kubectl\033[0m"
 curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.19.6/2021-01-05/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin
 kubectl version --short --client
+sleep 5
 
 #EKSCTL for AWS
+echo -e "\033[32mInstalling EKSCTL\033[0m"
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin
 eksctl version
+sleep 5
+
+echo -e "\033[32mAWS login\033[0m"
+sleep 2
+
+echo -e "\033[32mEnter AWS ACCESS KEY ID: \033[0m"
+read -r AWS_ACCESS_KEY_ID
+
+echo -e "\033[32mEnter AWS SECRET ACCESS KEY: \033[0m"
+read -s AWS_SECRET_ACCESS_KEY
+echo
+
+echo -e "\033[32mEnter AWS DEFAULT REGION (e.g., us-east-1): \033[0m"
+read -r AWS_DEFAULT_REGION
+
+# Configure AWS CLI
+aws configure set aws_access_key_id "$AWS_ACCESS_KEY_ID"
+aws configure set aws_secret_access_key "$AWS_SECRET_ACCESS_KEY"
+aws configure set default.region "$AWS_DEFAULT_REGION"
+aws configure set default.output_format json
+
+echo -e "\033[32mAWS configured successfully!\033[0m"
